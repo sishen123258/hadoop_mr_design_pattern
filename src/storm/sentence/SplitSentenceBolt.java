508bdfdef4b6df20330c020f2968d96b71ba9cfb
tong <1144299328@qq.com>
2016-01-31 18:20:38 +0800

    custom

---
+++ src/storm/sentence/SplitSentenceBolt.java
@@ -0,0 +1,39 @@
+package storm.sentence;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+import java.util.Map;
+
+/**
+ * Created by root on 1/31/16.
+ */
+public class SplitSentenceBolt extends BaseRichBolt{
+
+    private OutputCollector collector;
+
+    @Override
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+        this.collector=collector;
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+        String sentence=tuple.getStringByField("sentence");
+        String []words=sentence.split(" ");
+        for (String word:words){
+            this.collector.emit(new Values(word));
+        }
+
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        outputFieldsDeclarer.declare(new Fields("word"));
+    }
+}
